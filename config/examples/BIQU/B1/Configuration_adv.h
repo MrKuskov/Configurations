a5d78273f563d82cfccf2fbfbc97efb7bdd54af4
MrKuskov <94338763+MrKuskov@users.noreply.github.com>
2024-07-13 10:16:35 +0700

    Update Configuration_adv.h

--- config/examples/BIQU/B1/Configuration_adv.h
+++ config/examples/BIQU/B1/Configuration_adv.h
@@ -3379,12 +3379,12 @@
    */
   #define HYBRID_THRESHOLD
 
-  #define X_HYBRID_THRESHOLD     200 //100  // [mm/s]
+  #define X_HYBRID_THRESHOLD     352  // [mm/s]
   #define X2_HYBRID_THRESHOLD    100
-  #define Y_HYBRID_THRESHOLD     200 //100
+  #define Y_HYBRID_THRESHOLD     352
   #define Y2_HYBRID_THRESHOLD    100
-  #define Z_HYBRID_THRESHOLD       3
-  #define Z2_HYBRID_THRESHOLD      3
+  #define Z_HYBRID_THRESHOLD       15
+  #define Z2_HYBRID_THRESHOLD      15
   #define Z3_HYBRID_THRESHOLD      3
   #define Z4_HYBRID_THRESHOLD      3
   #define I_HYBRID_THRESHOLD       3  // [linear=mm/s, rotational=°/s]
